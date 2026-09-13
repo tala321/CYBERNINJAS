a6f290c305302d39f16e2be8e1bb3444d54aec7e
tala321 <168865947+tala321@users.noreply.github.com>
2026-09-13 05:17:42 +0200

    fix database connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,17 +1,18 @@
 import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+
 DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
     echo=False,
-    connect_args={
-        "charset": "utf8mb4"
-    }
 )
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -19,4 +20,13 @@ SessionLocal = sessionmaker(
     bind=engine
 )
 
+
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
