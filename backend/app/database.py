7732bda58f6c3afd794ea5aaae2461ba77e29a25
tala321 <168865947+tala321@users.noreply.github.com>
2026-09-13 03:58:02 +0200

    Fix database connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,57 +1,24 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL = (
-    "mysql+pymysql://root:TalaSaied@localhost/cyberninja_db"
-    "?charset=utf8mb4"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 engine = create_engine(
-
     DATABASE_URL,
-
     pool_pre_ping=True,
-
-    echo=False,
-
-    connect_args={
-
-        "charset": "utf8mb4",
-
-        "use_unicode": True
-
-    }
-
+    echo=False
 )
-
 
 
 SessionLocal = sessionmaker(
-
     autocommit=False,
-
     autoflush=False,
-
     bind=engine
-
 )
 
 
-
 Base = declarative_base()
-
-
-
-def get_db():
-
-    db = SessionLocal()
-
-    try:
-
-        yield db
-
-    finally:
-
-        db.close()
