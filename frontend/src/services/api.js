e91ab524c9a24bc281a3d67a5210303b0f6eb41d
tala321 <168865947+tala321@users.noreply.github.com>
2026-09-14 17:35:40 +0200

    fix production backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,7 +7,6 @@ const api = axios.create({
   },
 });
 
-// Add JWT token automatically to every request
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
